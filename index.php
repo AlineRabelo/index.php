fa42000c28d46500bb4683f08b200617dac32418
AlineRabelo <66781270+AlineRabelo@users.noreply.github.com>
2020-09-29 11:50:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>GITHUB Class</h1>
+
+
+    <?php 
+        echo '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>
+        Illo delectus perferendis cum, commodi quis odit ipsum voluptatum, maiores quod ea quisquam 
+        nostrum numquam odio ad nulla nisi architecto natus dolorem.</p>'
+    ?>
+    
+</body>
+</html>
